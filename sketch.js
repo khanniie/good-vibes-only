87e2c92357750fdacb737f24fe2a5a8f6af24d8d
Constance Ye <constanceye1316@gmail.com>
2017-10-23 12:09:06 -0400

    mostly done!!! yay!!1

--- sketch.js
+++
@@ -1,7 +0,0 @@
-function setup(){
-   createCanvas(710, 400);
-}
-
-function draw(){
-
-}
---
+++ unused/sketch.js
@@ -0,0 +1,6 @@
+function setup(){
+   var can = createCanvas(800, 600);
+   can.id("p5");
+   var c = color('rgba(0, 0, 255, 0.5)');
+   can.background(c);
+}
